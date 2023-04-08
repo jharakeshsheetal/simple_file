a67761051db9e23431a04c284657b24d82b88e80
jharakeshsheetal <130231502+jharakeshsheetal@users.noreply.github.com>
2023-04-08 19:41:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+// function hide_others(element){
+//     let home_section = document.getElementById("home_section")
+//     let science_section = document.getElementById("science_section")
+//     let technology_section = document.getElementById("technology_section")
+//     let engineering_section = document.getElementById("engineering_section")
+//     let maths_section = document.getElementById("maths_section")
+
+//     if (element == "home_section"){
+//         science_section.style.visibility = "hidden";
+//         technology_section.style.visibility = "hidden";
+//         engineering_section.style.visibility = "hidden";
+//         maths_section.style.visibility = "hidden";
+//     }
+    
+//     if (element == "science_section"){
+//         home_section.style.visibility = "hidden";
+//         technology_section.style.visibility = "hidden";
+//         engineering_section.style.visibility = "hidden";
+//         maths_section.style.visibility = "hidden";
+//     }
+    
+//     if (element == "technology_section"){
+//         science_section.style.visibility = "hidden";
+//         home_section.style.visibility = "hidden";
+//         engineering_section.style.visibility = "hidden";
+//         maths_section.style.visibility = "hidden";
+//     }
+    
+//     if (element == "engineering_section"){
+//         science_section.style.visibility = "hidden";
+//         technology_section.style.visibility = "hidden";
+//         home_section.style.visibility = "hidden";
+//         maths_section.style.visibility = "hidden";
+//     }
+    
+//     if (element == "maths_section"){
+//         science_section.style.visibility = "hidden";
+//         technology_section.style.visibility = "hidden";
+//         engineering_section.style.visibility = "hidden";
+//         home_section.style.visibility = "hidden";
+//     }
+    
+// }
+
+// function show_home(){
+//     hide_others("home_section")
+//     let a = document.getElementById("home_section")
+//     a.style.visibility = "visible";
+//     console.log("home")
+// }
+// function show_science(){
+//     hide_others("science_section")
+//     let a = document.getElementById("science_section")
+//     a.style.visibility = "visible";
+//     console.log("science_section")
+// }
+// function show_engineering(){
+//     hide_others("engineering_section")
+//     let a = document.getElementById("engineering_section")
+//     a.style.visibility = "visible";
+//     console.log("engineering_section")
+// }
+// function show_technology(){
+//     hide_others("technology_section")
+//     let a = document.getElementById("technology_section")
+//     a.style.visibility = "visible";
+//     console.log("technology_section")
+// }
+// function show_maths(){
+//     hide_others("maths_section")
+//     let a = document.getElementById("maths_section")
+//     a.style.visibility = "visible";
+//     console.log("maths_section")
+// }
